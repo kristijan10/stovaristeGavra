6c4d45fbcdb7ccc95a58b2cac64a3b3513ff72eb
Kristijan <kristijan@pop-os.localdomain>
2022-01-17 20:02:46 +0100

    Failed building due to missing attribute

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -17,7 +17,10 @@ export default function Footer() {
       </div>
       <div className="map__holder">
         <h4 className="map__title">Kako do nas</h4>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8245.877587007095!2d20.235892464485012!3d44.773398290110315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a698672c9ae8f%3A0x3ccde4226cfaf0b9!2sSTR%20%22Gavra%22%2C%20Stovari%C5%A1te%20Ogreva%2C%20Uglja%2C%20Drva.!5e0!3m2!1ssr!2srs!4v1642441065250!5m2!1ssr!2srs"></iframe>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8245.877587007095!2d20.235892464485012!3d44.773398290110315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a698672c9ae8f%3A0x3ccde4226cfaf0b9!2sSTR%20%22Gavra%22%2C%20Stovari%C5%A1te%20Ogreva%2C%20Uglja%2C%20Drva.!5e0!3m2!1ssr!2srs!4v1642441065250!5m2!1ssr!2srs"
+          title="googleMap"
+        ></iframe>
       </div>
       <div className="contact__holder">
         <h4 className="contact__title">Kontaktirajte nas</h4>
